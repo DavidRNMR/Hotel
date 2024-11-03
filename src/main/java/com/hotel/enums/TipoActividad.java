689d68174eb3c33f8e03b0548dffa15a6aab52a9
DESKTOP-T9P3FJ4\Usuario <uniandrome@hotmail.com>
2024-11-03 20:49:27 +0100

    se añaden actividades a la reserva

--- src/main/java/com/hotel/enums/TipoActividad.java
+++ src/main/java/com/hotel/enums/TipoActividad.java
@@ -7,9 +7,12 @@ import java.math.BigDecimal;
 @Getter
 public enum TipoActividad {
 
+    SPA(new BigDecimal("60.00")),
+    GIMNASIO(new BigDecimal("00.00")),
+    AQUAGYM(new BigDecimal("00.00")),
     SURF(new BigDecimal("100.00")),
-    GIMNASIO(new BigDecimal("50.00")),
-    SPA(new BigDecimal("50.00"));
+    SENDERISMO(new BigDecimal("30.00")),
+    SNORKELING(new BigDecimal("75.00"));
 
     private BigDecimal precio;
     TipoActividad (BigDecimal precio){
