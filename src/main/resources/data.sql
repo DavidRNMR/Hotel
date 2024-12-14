27874bcd0b6bea1d96be1b59f06e1d6d42ed3c03
DESKTOP-T9P3FJ4\Usuario <uniandrome@hotmail.com>
2024-12-14 17:54:01 +0100

    automatizando script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
-INSERT INTO habitacion_tipo (tipo, precio, cantidad_total)
-VALUES ('DOBLE', 100.00, 20);
+INSERT INTO habitacion_tipo (tipo, precio, cantidad_total) VALUES ('DOBLE', 100.00, 20);
 
 INSERT INTO habitacion_tipo (tipo, precio, cantidad_total)
 VALUES ('TRIPLE', 150.00, 10);
